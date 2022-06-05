904838ec214abea3a188c2a83411f958eb8199e8
restevean <restevean@protonmail.com>
2022-06-05 14:42:51 +0200

    Permissions on POST lambda already running correctly

--- lambda/actions.py
+++ lambda/actions.py
@@ -1,5 +1,5 @@
 import json
-# import boto3
+import boto3
 # from typing import Dict
 # from aws_lambda_context import LambdaContext
 # from botocore.exceptions import ClientError
@@ -14,56 +14,33 @@ def lambda_handler_0(event, context):
         }
     else:
         return {'statusCode': 403, 'body': 'Not Allowed'}
-    # if method == 'GET':
-    #     lambda_1(event)
-    # elif method == 'POST':
-    #     lambda_2(event)
-
-
-# def lambda_handler_1(event, context):
-#     return {
-#         'statusCode': 200,
-#         'body': json.dumps(event)
-#         # 'body': json.dumps(event["pathParameters"])
-#         # 'body':
-#         #     event["queryStringParameters"]['key1'] +
-#         #     ". " +
-#         #     event["queryStringParameters"]['key2'] +
-#         #     ". " +
-#         #     event["queryStringParameters"]['key3'] +
-#         #     ". Hello CDK Watch!, changes"
-#     }
 
 
 def lambda_handler_1(event, context):
     return {'statusCode': 200, 'body': 'Handler 1, method "GET"'}
 
 
+# TODO: Permissions
 def lambda_handler_2(event, context):
-    return {'statusCode': 200, 'body': 'Handler 2, method "POST"'}
+    s3 = boto3.resource('s3')
+    bucket_name = event["queryStringParameters"]['bucket']
+    object_name = event["queryStringParameters"]['object']
+    bucket = s3.Bucket(bucket_name)
+    objs = list(bucket.objects.filter(Prefix=object_name))
 
+    if len(objs) <= 0:
+        bucket.put_object(Bucket=bucket_name, Key=object_name)
+        return {
+            'statusCode': 201,
+            'body': json.dumps(f'Created object {object_name} on {bucket_name}')
+            # 'body': json.dumps(event)
+        }
+    else:
+        return {
+            'statusCode': 409,
+            'body': json.dumps(f'object {object_name} already exists on {bucket_name}')
+            # 'body': json.dumps(event)
+        }
 
 def lambda_handler_3(event, context):
     return {'statusCode': 200, 'body': 'Handler 3, method "PUT"'}
-
-# def lambda_2(event: Dict):
-#     s3 = boto3.resource('s3')
-#     # bucket_name = event["queryStringParameters"]['bucket']
-#     bucket_name = 'restevean-first-bucket'
-#     object_name = event["queryStringParameters"]['object']
-#     bucket = s3.Bucket(bucket_name)
-#     objs = list(bucket.objects.filter(Prefix=object_name))
-#
-#     if len(objs) <= 0:
-#         bucket.put_object(Bucket=bucket_name, Key=object_name)
-#         return {
-#             'statusCode': 201,
-#             'body': json.dumps(f'Created object {object_name} on {bucket_name}')
-#             # 'body': json.dumps(event)
-#         }
-#     else:
-#         return {
-#             'statusCode': 409,
-#             'body': json.dumps(f'object {object_name} already exists on {bucket_name}')
-#             # 'body': json.dumps(event)
-#         }
--- sidikey/sidikey_stack.py
+++ sidikey/sidikey_stack.py
@@ -4,10 +4,11 @@ from aws_cdk import (
     aws_lambda as _lambda,
     aws_apigateway as apigw,
     aws_s3 as s3,
+    aws_iam as iam,
 )
 
 
-# TODO: s3 use an existing bucket, permissions, layers, , testing, poetry
+# TODO: s3 use an existing bucket, layers, testing, poetry,
 
 
 class SidikeyStack(Stack):
@@ -15,40 +16,44 @@ class SidikeyStack(Stack):
     def __init__(self, scope: Construct, id_: str, **kwargs) -> None:
         super().__init__(scope, id_, **kwargs)
 
-        # Defines an AWS Lambda resource
+        # Defines an AWS Lambda resource, my_lambda_0
         my_lambda_0 = _lambda.Function(
-            self, 'Handler_0',
+            self, 'handler_0',
             runtime=_lambda.Runtime.PYTHON_3_9,
             code=_lambda.Code.from_asset('lambda'),
-            description='Handler 0, root of Sidikey API gateway',
+            description='handler 0, root of Sidikey API gateway',
             handler='actions.lambda_handler_0',
+            function_name='handler_root'
         )
 
-        # Defines an AWS Lambda resource
+        # Defines an AWS Lambda resource, my_lambda_1
         my_lambda_1 = _lambda.Function(
-            self, 'Handler_1',
+            self, 'handler_1',
             runtime=_lambda.Runtime.PYTHON_3_9,
             code=_lambda.Code.from_asset('lambda'),
             description='Handler 1, method "GET" on resource /testing',
             handler='actions.lambda_handler_1',
+            function_name='testing_get'
         )
 
-        # Defines an AWS Lambda resource
+        # Defines an AWS Lambda resource, my_lambda_2
         my_lambda_2 = _lambda.Function(
-            self, 'Handler_2',
+            self, 'handler_2',
             runtime=_lambda.Runtime.PYTHON_3_9,
             code=_lambda.Code.from_asset('lambda'),
-            description='Handler 1, method "POST" on resource /testing',
+            description='Handler 2, method "POST" on resource /testing',
             handler='actions.lambda_handler_2',
+            function_name='testing_post'
         )
 
-        # Defines an AWS Lambda resource
+        # Defines an AWS Lambda resource, my_lambda_3
         my_lambda_3 = _lambda.Function(
-            self, 'Handler_3',
+            self, 'handler_3',
             runtime=_lambda.Runtime.PYTHON_3_9,
             code=_lambda.Code.from_asset('lambda'),
-            description='Handler 1, method "PUT" on resource /testing',
+            description='Handler 3, method "PUT" on resource /testing',
             handler='actions.lambda_handler_3',
+            function_name='testing_put'
         )
 
         # Defines an AWS API Gateway resource and assign as a trigger for my_lambda_0
@@ -69,8 +74,26 @@ class SidikeyStack(Stack):
         items.add_method("PUT", put_method_integration)  # PUT /items
 
         # Defines an AWS s3 bucket to create (does not exist until deployment)
-        s3.Bucket(
+        bucket = s3.Bucket(
             self,
             'restevean-cdk-bucket',
             bucket_name='restevean-cdk-bucket'
+        )
+
+        # Defines and add permissions to my_lambda_2
+        my_lambda_2.add_to_role_policy(iam.PolicyStatement(
+            actions=["s3:ListBucket"],
+            effect=iam.Effect.ALLOW,
+            resources=['arn:aws:s3:::restevean-cdk-bucket']
+            )
+        )
+
+        # Gives read/write permission from my_lambda_2 to the bucket
+        # bucket.grant_read_write(my_lambda_2)
+
+        my_lambda_2.add_to_role_policy(iam.PolicyStatement(
+            actions=["s3:PutObject"],
+            effect=iam.Effect.ALLOW,
+            resources=['arn:aws:s3:::restevean-cdk-bucket/*']
+            )
         )
