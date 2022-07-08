5da382a6239fef18c59db4cbbabb992765497c30
restevean <restevean@protonmail.com>
2022-07-08 15:02:55 +0200

    Cognito implemented

--- cdk_stacks/sidikey_stack.py
+++ cdk_stacks/sidikey_stack.py
@@ -5,11 +5,14 @@ from aws_cdk import (
     aws_apigateway as apigw,
     aws_s3 as s3,
     aws_iam as iam,
+    aws_cognito as cognito,
 )
 from typing import Any
 
 
-# TODO: s3 use an existing bucket, testing
+# TODO: s3 use an existing bucket (do not create bucket if exist)
+# TODO: testing
+# TODO: Custom domain in stack
 
 
 class SidikeyStack(Stack):
@@ -73,9 +76,101 @@ class SidikeyStack(Stack):
             self, 'Sidikey',
             handler=my_lambda_0,
             proxy=False,
+            # default_domain_mapping={"api.restevean.es": dn},
         )
 
-        api.root.add_method("GET")
+        # Defines cognito user pool
+        user_pool = cognito.UserPool(self, 'MyUserPool05',
+                                     user_pool_name='Sidikey_User_Pool',
+                                     standard_attributes=cognito.StandardAttributes(
+                                         email=cognito.StandardAttribute(
+                                             required=True,
+                                             mutable=True
+                                         )
+                                     ),
+                                     self_sign_up_enabled=False,
+                                     auto_verify=cognito.AutoVerifiedAttrs(email=True, phone=False),
+                                     account_recovery=cognito.AccountRecovery.EMAIL_ONLY
+                                     )
+
+        # Assign an app client to created cognito pool
+        user_pool_cli = user_pool.add_client("SidikeyApp01",
+                                             o_auth=cognito.OAuthSettings(
+                                                 flows=cognito.OAuthFlows(
+                                                     authorization_code_grant=True,
+                                                     implicit_code_grant=True,
+                                                 ),
+                                                 scopes=[cognito.OAuthScope.EMAIL],
+                                                 callback_urls=["https://api.restevean.es"],
+                                                 logout_urls=["https://api.restevean.es"]
+                                             ),
+                                             )
+
+        # Assign a domain to the user pool
+        user_pool_domain = user_pool.add_domain("resteveandomain13",
+                                                cognito_domain=cognito.CognitoDomainOptions(
+                                                    domain_prefix="sidikeydomain"
+                                                ),
+                                                )
+
+        # Create a user Admin and attach it to the cognito_user_admin_group
+        cognito.CfnUserPoolUser(self, "Admin",
+                                user_pool_id=user_pool.user_pool_id,
+                                username="Admin",
+                                # desired_delivery_mediums=["desiredDeliveryMediums"],
+                                # message_action="RESEND",
+                                user_attributes=[cognito.CfnUserPoolUser.AttributeTypeProperty(
+                                    name="email",
+                                    value="resteve24@gmail.com"
+                                )],
+                                )
+
+        cognito.CfnUserPoolGroup(self, "my_admin_users",
+                                 user_pool_id=user_pool.user_pool_id,
+                                 description="Can create users for thr pool",
+                                 group_name="admin_users",
+                                 precedence=1,
+                                 role_arn=cognito_admin_user_role.role_arn
+                                 )
+
+        cognito.CfnUserPoolUserToGroupAttachment(self, "MyCfnUserPoolUserToGroupAttachment",
+                                                 group_name="admin_users",
+                                                 username="Admin",
+                                                 user_pool_id=user_pool.user_pool_id
+                                                 )
+
+        user_pool_domain.sign_in_url(user_pool_cli,
+
+                                     redirect_uri="https://api.restevean.es"
+        # Create a role
+        cognito_admin_user_role = iam.Role(self, "Role",
+                                           assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
+                                           description="Can add user to the pool"
+                                           )
+
+        # Set up role permissions
+        cognito_admin_user_role.add_to_policy(iam.PolicyStatement(
+            actions=[
+                "cognito-idp:AdminEnableUser",
+                "cognito-idp:AdminCreateUser",
+                "cognito-idp:AdminDisableUser"
+            ],
+            resources=[user_pool.user_pool_arn]
+        ))
+
+        # Creates an authorizer
+        my_authorizer = apigw.CognitoUserPoolsAuthorizer(self, "SidikeyAuthorizer",
+                                                         cognito_user_pools=[user_pool],
+                                                         )
+
+        # Creating methods
+        # api.root.add_method("GET", )
+        api.root.add_method("GET", apigw.HttpIntegration("https://api.restevean.es"),
+                            # authorizer=authorizer,
+                            authorizer=my_authorizer,
+                            authorization_type=apigw.AuthorizationType.COGNITO,
+                            )
+
         api.root.add_method("POST")
         new_resource = api.root.add_resource("testing")
         get_method_integration = apigw.LambdaIntegration(my_lambda_1)
@@ -85,8 +180,7 @@ class SidikeyStack(Stack):
         new_resource.add_method("POST", post_method_integration)  # POST /items
         new_resource.add_method("PUT", put_method_integration)  # PUT /items
 
-        # Defines an AWS s3 bucket to create (does not exist until deployment)
-        # bucket = s3.Bucket(
+        # Defines an AWS s3 bucket to create (if does not exist)
         s3.Bucket(
             self,
             'restevean-cdk-bucket',
@@ -103,7 +197,7 @@ class SidikeyStack(Stack):
             actions=["s3:ListBucket", "s3:PutObject"],
             effect=iam.Effect.ALLOW,
             resources=['arn:aws:s3:::restevean-cdk-bucket', 'arn:aws:s3:::restevean-cdk-bucket/*']
-            )
+        )
         )
 
         """
